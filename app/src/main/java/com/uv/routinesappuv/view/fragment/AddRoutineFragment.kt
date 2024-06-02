dc26a2cf9786a188f22573cd62dca81e0ddf8491
Juan José Moreno <juan.moreno.jaramillo@correounivalle.edu.cogit config --global user.email juan.moreno.jaramillo@correounivalle.edu.cogit config --global user.email juan.moreno.jaramillo@correounivalle.edu.co>
2024-06-02 18:29:58 -0500

    En progreso El back de Agregar Rutina

--- app/src/main/java/com/uv/routinesappuv/view/fragment/AddRoutineFragment.kt
+++ app/src/main/java/com/uv/routinesappuv/view/fragment/AddRoutineFragment.kt
@@ -1,10 +1,12 @@
 package com.uv.routinesappuv.view.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.uv.routinesappuv.R
 import com.uv.routinesappuv.databinding.FragmentAddRoutineBinding
 
@@ -15,8 +17,35 @@ class AddRoutineFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentAddRoutineBinding.inflate(inflater)
+        binding = FragmentAddRoutineBinding.inflate(inflater, container, false)
         binding.lifecycleOwner = this
+
+        setupToolbar()
+        setupSpinners()
+
         return binding.root
+    }
+
+    private fun setupToolbar() {
+        binding.contentToolbar.toolbarAddRoutine.setNavigationOnClickListener { onBackPressed() }
+    }
+    private fun onBackPressed() {
+        findNavController().navigate(R.id.action_go_back_home)
+    }
+
+    private fun setupSpinners() {
+        // Valores de prueba para los Spinners
+        val exercises = arrayOf("Nombre ejercicio","Push Up", "Pull Up", "Squat", "Deadlift")
+        val equipment = arrayOf("Equipamento", "Dumbbells", "Barbell", "Resistance Bands")
+
+        // Configurar adaptador para el Spinner de ejercicios
+        val exercisesAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, exercises)
+        exercisesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.inputNombreEjercicio.adapter = exercisesAdapter
+
+        // Configurar adaptador para el Spinner de equipamiento
+        val equipmentAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, equipment)
+        equipmentAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.inputEquipamiento.adapter = equipmentAdapter
     }
 }
