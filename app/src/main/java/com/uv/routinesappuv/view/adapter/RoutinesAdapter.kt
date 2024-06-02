d4b5cfa3407f9c0af49b3a30231d55dc49ed8ff3
Eyder Santiago Suarez Chavez <eyder.suarez@correounivalle.edu.co>
2024-06-02 17:12:08 -0500

    front edit recycleview 1

--- app/src/main/java/com/uv/routinesappuv/view/adapter/RoutinesAdapter.kt
+++ app/src/main/java/com/uv/routinesappuv/view/adapter/RoutinesAdapter.kt
@@ -3,6 +3,27 @@ package com.uv.routinesappuv.view.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.navigation.NavController
+import androidx.recyclerview.widget.RecyclerView
+import com.uv.routinesappuv.databinding.ItemExerciseBinding
+import com.uv.routinesappuv.model.Ejercicio
 import com.uv.routinesappuv.view.viewholder.RoutinesViewHolder
 
-class RoutinesAdapter()
+class RoutinesAdapter(private val listExercises: MutableList<Ejercicio>): RecyclerView.Adapter<RoutinesViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RoutinesViewHolder {
+        val binding = ItemExerciseBinding.inflate(LayoutInflater.from(parent.context),parent, false)
+        return RoutinesViewHolder(binding)
+    }
+
+    //retorno el tamaño de la lista
+    override fun getItemCount(): Int {
+        return listExercises.size
+    }
+
+    //detecta en dónde hice touch
+    override fun onBindViewHolder(holder: RoutinesViewHolder, position: Int) {
+        val inventory = listExercises[position]
+        holder.setItemExercise(inventory)
+    }
+
+}
--- app/src/main/java/com/uv/routinesappuv/view/viewholder/RoutinesViewHolder.kt
+++ app/src/main/java/com/uv/routinesappuv/view/viewholder/RoutinesViewHolder.kt
@@ -6,6 +6,8 @@ import androidx.navigation.NavController
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.uv.routinesappuv.R
+import com.uv.routinesappuv.databinding.ItemExerciseBinding
+import com.uv.routinesappuv.model.Ejercicio
 import com.uv.routinesappuv.utils.Constants
 import com.uv.routinesappuv.webService.ApiService
 import com.uv.routinesappuv.webService.ImageResponse
@@ -15,4 +17,24 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class RoutinesViewHolder()
+class RoutinesViewHolder(binding: ItemExerciseBinding) : RecyclerView.ViewHolder(binding.root){
+
+    val bindingExerciseItem = binding
+
+    fun setItemExercise(exercise: Ejercicio){
+        bindingExerciseItem.tvNombreEjercicio.text = exercise.nombre_ejercicio
+        bindingExerciseItem.tvDescripcion.text = exercise.descripcion_ejercicio
+        bindingExerciseItem.tvEquipamento.text = exercise.equipamento
+        bindingExerciseItem.tvSeries.text = "${exercise.series}"
+        bindingExerciseItem.tvRepeticiones.text = "${exercise.repeticiones}"
+
+//        bindingExerciseItem.cvRutina.setOnClickListener{
+//            val bundle = Bundle()
+//            bundle.putSerializable("clave", exercise)
+//        }
+
+    }
+
+
+
+}
