7f635fe152a08aa6134772446a5a2b45690269c8
Juan José Moreno <juan.moreno.jaramillo@correounivalle.edu.cogit config --global user.email juan.moreno.jaramillo@correounivalle.edu.cogit config --global user.email juan.moreno.jaramillo@correounivalle.edu.co>
2024-06-03 11:46:16 -0500

    Correcciones y validaciones en agregar rutina

--- app/src/main/java/com/uv/routinesappuv/view/fragment/AddRoutineFragment.kt
+++ app/src/main/java/com/uv/routinesappuv/view/fragment/AddRoutineFragment.kt
@@ -1,6 +1,8 @@
 package com.uv.routinesappuv.view.fragment
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -17,7 +19,6 @@ import com.uv.routinesappuv.webService.RoutinesResponse
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
 
 class AddRoutineFragment : Fragment() {
     private lateinit var binding: FragmentAddRoutineBinding
@@ -31,10 +32,13 @@ class AddRoutineFragment : Fragment() {
         binding = FragmentAddRoutineBinding.inflate(inflater, container, false)
         binding.lifecycleOwner = this
 
-        setupToolbar()
         // Initialize apiService before calling fetchExercises
         apiService = ApiUtils.getApiService()
         fetchExercises()
+
+        setupToolbar()
+        setupSpinners()
+        setupListeners()
 
         return binding.root
     }
@@ -90,11 +94,89 @@ class AddRoutineFragment : Fragment() {
         binding.inputNombreEjercicio.adapter = exercisesAdapter
 
         // Valores de prueba para el Spinner de equipamiento
-        val equipment = arrayOf("Equipamento", "Dumbbells", "Barbell", "Resistance Bands")
+        val equipment = arrayOf(
+            "Equipamento",
+            "Cinta de correr (treadmill)",
+            "Bicicleta estática",
+            "Elíptica",
+            "Máquina de remo",
+            "Stepper o escaladora",
+            "Cuerda para saltar",
+            "Máquina de esquí de fondo (ski erg)",
+            "Pesas libres (mancuernas y barras)",
+            "Kettlebells",
+            "Barras de dominadas",
+            "Bandas de resistencia",
+            "Máquinas de pesas (multi-gimnasio)",
+            "Bancos de pesas ajustables",
+            "Balones medicinales",
+            "Sacos de arena (sandbags)",
+            "Barras para pesas olímpicas y discos",
+            "Cajas pliométricas",
+            "TRX o sistemas de entrenamiento en suspensión",
+            "Rueda abdominal",
+            "Cuerdas de batalla (battle ropes)",
+            "Balón suizo (fitball)",
+            "Esterilla de yoga o colchoneta",
+            "Rodillos de espuma (foam rollers)",
+            "Barras de estiramiento",
+            "Bloques y correas de yoga",
+            "Cojines de equilibrio (bosu)",
+            "Sin equipamento"
+        )
 
         // Configurar adaptador para el Spinner de equipamiento
         val equipmentAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, equipment)
         equipmentAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         binding.inputEquipamiento.adapter = equipmentAdapter
     }
+
+    private fun setupListeners() {
+        val textWatchers = listOf(
+            binding.etDescripcion,
+            binding.etSeries,
+            binding.etRepeticiones,
+            binding.etNombreRutina,
+            binding.etDescripcion
+        )
+
+        for (textView in textWatchers) {
+            textView.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    validateFields()
+                }
+                override fun afterTextChanged(s: Editable?) {}
+            })
+        }
+
+        binding.btnAgregarEjercicio.setOnClickListener {
+            if (binding.inputNombreEjercicio.selectedItem == "Nombre ejercicio" ||
+                binding.inputEquipamiento.selectedItem == "Equipamento") {
+                Toast.makeText(requireContext(), "Selecciona un nombre del ejercicio o equipamento", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(requireContext(), "FUNCIONO", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.btnAgregarRutina.setOnClickListener {
+            if (binding.etNombreRutina.text.toString().isEmpty() ||
+                binding.etDescripcion.text.toString().isEmpty()) {
+                Toast.makeText(requireContext(), "Por favor, completa todos los campos", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(requireContext(), "FUNCIONO RUTINA", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+    private fun validateFields() {
+        val isDescripcionFilled = binding.etDescripcion.text.toString().isNotEmpty()
+        val isSeriesFilled = binding.etSeries.text.toString().isNotEmpty()
+        val isRepeticionesFilled = binding.etRepeticiones.text.toString().isNotEmpty()
+        val isNombreRutinaFilled = binding.etNombreRutina.text.toString().isNotEmpty()
+        val isDescripcionRutinaFilled = binding.etDescripcionRutina.text.toString().isNotEmpty()
+
+        binding.btnAgregarEjercicio.isEnabled = isDescripcionFilled && isSeriesFilled && isRepeticionesFilled
+        binding.btnAgregarRutina.isEnabled = isNombreRutinaFilled && isDescripcionRutinaFilled
+    }
+
 }
