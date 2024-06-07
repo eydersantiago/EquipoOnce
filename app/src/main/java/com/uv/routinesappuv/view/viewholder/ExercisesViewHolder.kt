33d0c9c3c4ae50596c4e921e3485b0c78c053f63
valentinabarbetty <valentinabarbetty2@gmail.com>
2024-06-06 23:08:57 -0500

    detalles de la rutina

--- app/src/main/java/com/uv/routinesappuv/view/viewholder/ExercisesViewHolder.kt
+++ app/src/main/java/com/uv/routinesappuv/view/viewholder/ExercisesViewHolder.kt
@@ -1,32 +1,15 @@
 package com.uv.routinesappuv.view.viewholder
 
-import android.os.Bundle
-import androidx.navigation.NavController
 import androidx.recyclerview.widget.RecyclerView
-import com.uv.routinesappuv.R
 import com.uv.routinesappuv.databinding.ItemExerciseBinding
 import com.uv.routinesappuv.model.Ejercicio
 
-class ExercisesViewHolder(binding: ItemExerciseBinding) : RecyclerView.ViewHolder(binding.root){
-
-    val bindingExerciseItem = binding
-
-    fun setItemExercise(exercise: Ejercicio){
-        bindingExerciseItem.tvNombreEjercicio.text = exercise.nombre_ejercicio
-        bindingExerciseItem.tvDescripcion.text = exercise.descripcion_ejercicio
-        bindingExerciseItem.tvEquipamento.text = exercise.equipamento
-        bindingExerciseItem.tvSeries.text = "${exercise.series}"
-        bindingExerciseItem.tvRepeticiones.text = "${exercise.repeticiones}"
-
-//        bindingExerciseItem.cvRutina.setOnClickListener{
-//            val bundle = Bundle()
-//            bundle.putSerializable("clave", exercise)navController.navigate(
-//                    R.id.action_add_routine, bundle
-//            )
-//        }
-
+class ExercisesViewHolder(private val binding: ItemExerciseBinding) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(ejercicio: Ejercicio) {
+        binding.tvNombreEjercicio.text = ejercicio.nombre_ejercicio
+        binding.tvDescripcion.text = ejercicio.descripcion_ejercicio
+        binding.tvEquipamento.text = ejercicio.equipamento
+        binding.tvSeries.text = "Series: ${ejercicio.series}"
+        binding.tvRepeticiones.text = "Repeticiones: ${ejercicio.repeticiones}"
     }
-
-
-
 }
