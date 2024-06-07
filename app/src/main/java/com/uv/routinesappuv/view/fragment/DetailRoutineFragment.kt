de8af8e13d24a12ffaf320ae71841e4eb688c3b6
valentinabarbetty <valentinabarbetty2@gmail.com>
2024-06-07 12:03:18 -0500

    Titulo toolbar de detalle rutina

--- app/src/main/java/com/uv/routinesappuv/view/fragment/DetailRoutineFragment.kt
+++ app/src/main/java/com/uv/routinesappuv/view/fragment/DetailRoutineFragment.kt
@@ -5,14 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.uv.routinesappuv.databinding.FragmentDetailRoutineBinding
 import com.uv.routinesappuv.model.Rutina
 import com.uv.routinesappuv.view.adapter.ExercisesAdapter
 import com.uv.routinesappuv.viewmodel.RoutinesViewModel
-
+import com.uv.routinesappuv.R
 class DetailRoutineFragment : Fragment() {
     private lateinit var binding: FragmentDetailRoutineBinding
 
@@ -32,6 +34,7 @@ class DetailRoutineFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         dataRutina()
         controladores()
+        setupToolbar()
     }
 
     private fun controladores() {
@@ -48,5 +51,14 @@ class DetailRoutineFragment : Fragment() {
 
         binding.titleDescripcion.text = receivedRutina.descripcion_rutina
 
+        // Aquí se actualiza el TextView del Toolbar
+        val toolbarTitle = view?.findViewById<TextView>(R.id.titleTextViewEdit)
+        toolbarTitle?.text = receivedRutina.nombre_rutina // Suponiendo que el nombre de la rutina está en esta variable
+    }
+    private fun setupToolbar() {
+        binding.contentToolbarEditar.toolbarEdit.setNavigationOnClickListener { onBackPressed() }
+    }
+    private fun onBackPressed() {
+        findNavController().navigate(R.id.action_fragment_detail_routine_to_fragment_home_routine)
     }
 }
