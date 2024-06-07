01b58af767de7508a003392aa47ddf33a12567f6
Javier <javier.posso@correounivalle.edu.co>
2024-06-07 13:27:50 -0500

    Added back button function

--- app/src/main/java/com/uv/routinesappuv/view/MainActivity.kt
+++ app/src/main/java/com/uv/routinesappuv/view/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.databinding.DataBindingUtil
 import com.google.firebase.FirebaseApp
 import com.uv.routinesappuv.R
 import com.uv.routinesappuv.databinding.ActivityMainBinding
+import androidx.activity.OnBackPressedDispatcherOwner
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -14,5 +15,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         FirebaseApp.initializeApp(this)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        supportFragmentManager.addOnBackStackChangedListener {
+            if (supportFragmentManager.backStackEntryCount == 0) {
+                // Cuando no hay fragments en la pila de retroceso, finaliza la actividad
+                finish()
+            }
+        }
     }
 }
