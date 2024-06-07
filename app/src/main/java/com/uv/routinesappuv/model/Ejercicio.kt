c39d07a3842ac0287e47d9f58ad69a71b836d44b
Eyder Santiago Suarez Chavez <eyder.suarez@correounivalle.edu.co>
2024-06-06 23:43:36 -0500

    corrección de merge

--- app/src/main/java/com/uv/routinesappuv/model/Ejercicio.kt
+++ app/src/main/java/com/uv/routinesappuv/model/Ejercicio.kt
@@ -1,12 +1,10 @@
 package com.uv.routinesappuv.model
 
-import java.io.Serializable
-
 data class Ejercicio (
-    val id: String,
+    val id: Int,
     val nombre_ejercicio: String,
     val descripcion_ejercicio: String,
     val equipamento: String,
     val series: Int,
     val repeticiones: Int
-): Serializable
+)
